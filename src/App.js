f7fe07281977b39f81e15eab32ad9a8a3b08ca24
SerhiyStepanov <63470231+SerhiyStepanov@users.noreply.github.com>
2021-05-23 19:35:25 +0300

    svg

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import Stamp from "./image/stamp.svg";
 import s from "./app.module.css";
 
 export default function App() {
   return (
     <section className={s.wrapper}>
       <div className={s.thumb}>
-        <header className={s.header}>
-          <Stamp />
-          Stamps of different countries.
-        </header>
+        <svg className={s.icon}>
+          <use href="stamp.svg"></use>
+        </svg>
+        <header className={s.header}>Stamps of different countries.</header>
       </div>
     </section>
   );
