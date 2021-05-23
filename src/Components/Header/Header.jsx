aac5f10868165bba7e2b11c940feac16cebee270
SerhiyStepanov <63470231+SerhiyStepanov@users.noreply.github.com>
2021-05-23 21:30:38 +0300

    add text

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,12 +2,20 @@ import s from "./header.module.css"
 
 export default function Header() {
     return (
+      <section>
         <div className={s.thumb}>
         <img src="stamp.svg" alt="post" className={s.stamp} />
 
-        <header className={s.header}>Stamps of different countries.</header>
+        <h4 className={s.header}>Stamps of different countries.</h4>
 
         <img src="stamp.svg" alt="post" className={s.stamp} />
+
+        
       </div>
+
+      <p className={s.text}>Good day. If you are interested in old postage stamps, you will be interested to look at my albums.
+        These albums contain postage stamps of different countries and times. For those interested in buying, I provide telephone and email.
+        Enjoy.</p>
+        </section>
     )
 }
