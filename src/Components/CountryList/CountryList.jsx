a6677b8da635c64a037799de04fa6662e8df0309
SerhiyStepanov <63470231+SerhiyStepanov@users.noreply.github.com>
2021-05-27 01:01:46 +0300

    add title

--- src/Components/CountryList/CountryList.jsx
+++ src/Components/CountryList/CountryList.jsx
@@ -6,6 +6,7 @@ export default function CountryList() {
     return (
         <section className={s.wrapper}>
             <ul className={s.thumb}>
+                <h3 className={s.title}>Select a country.</h3>
                 {countrys.map((el, index) => (
                     <li key={index} className={s.list}>
                         <a  href={el.url}
