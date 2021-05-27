f5c3da3905430baedb8428074a00162d9074662e
SerhiyStepanov <63470231+SerhiyStepanov@users.noreply.github.com>
2021-05-27 11:58:04 +0300

    change color link

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -16,8 +16,7 @@ export default function Header() {
       </div>
 
       <p className={s.text}>Good day. If you are interested in old postage stamps, you will be interested to look at my albums.
-        These albums contain postage stamps of different countries and times. For those interested in buying, I provide telephone and email.
-        Enjoy.</p>
+        These albums contain postage stamps of different countries and times. For those interested in buying, I provide telephone and email.</p>
 
         
       </section>
