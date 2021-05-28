b1edb52e479f72cc19b9456ded5b72b15f0e40a3
SerhiyStepanov <63470231+SerhiyStepanov@users.noreply.github.com>
2021-05-28 15:42:04 +0300

    add sort

--- src/Components/CountryList/CountryList.jsx
+++ src/Components/CountryList/CountryList.jsx
@@ -1,18 +1,31 @@
 import countrys from "../../countrys.json"
 import s from "./countryList.module.css"
 
+
+const sortCountrys =  countrys.sort(function (a, b) {
+  if (a.country > b.country) {
+    return 1;
+  }
+  if (a.country < b.country) {
+    return -1;
+  }
+
+  return 0;
+});
+
+
 export default function CountryList() {
-    console.log(countrys)
+
     return (
         <section className={s.wrapper}>
             <ul className={s.thumb}>
                 <h3 className={s.title}>Select a country.</h3>
-                {countrys.map((el, index) => (
+                {sortCountrys.map((el, index) => (
                     <li key={index} className={s.list}>
-                        <a  href={el.url}
-                  className={s.link}
-                  target="_blank"
-                            rel="nofollow">{ el["country "]}</a>
+                        <a href={el.url}
+                            className={s.link}
+                            target="_blank"
+                            rel="nofollow">{el["country"]}</a>
                     </li>
                 ))}
             </ul>
