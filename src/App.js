dbf4995a9087efd786c0aaa3a80f1ba131509cfe
SerhiyStepanov <63470231+SerhiyStepanov@users.noreply.github.com>
2021-06-04 17:49:53 +0300

    add Contact

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import Header from "./Components/Header";
 import CountryList from "./Components/CountryList";
 import s from "./app.module.css";
+import Footer from "./Components/Footer/Footer";
 
 export default function App() {
   return (
-    <section className={s.wrapper}>
-      <Header />
+    <>
+      <section className={s.wrapper}>
+        <Header />
 
-      <CountryList />
-    </section>
+        <CountryList />
+
+        <Footer />
+      </section>
+    </>
   );
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import s from "./footer.module.css"
+
+export default function Footer() {
+    return (
+        <section className={s.wrapper}>
+            <div className={s.thumb}>
+                <b>Contact:</b>
+                <a href="" className={s.link}>a a a</a>
+            </div>
+        </section>
+    )
+}
