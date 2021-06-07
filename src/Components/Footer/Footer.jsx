00f1daad9c4ef042909fde9dfffb3e77c44ff688
SerhiyStepanov <63470231+SerhiyStepanov@users.noreply.github.com>
2021-06-07 19:15:55 +0300

    add link email

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <section className={s.wrapper}>
             <div className={s.thumb}>
                 <b>Contact:</b>
-                <a href="" className={s.link}>a a a</a>
+                <a href="mailto:serhiystepanov@ukr.net" target="_blank" rel="noreferrer" className={s.link}>serhiystepanov@ukr.net</a>
             </div>
         </section>
     )
